dced2b4b7eef9cb953e1890ebee8e594a393122f
Lyra7770 <lyramir955@gmail.com>
2025-06-10 12:05:55 -0400

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 let luces = [];
-let cantidad = 80;
+let cantidad;
 
-// Paleta exacta de colores
 let paleta = [
-  [34, 41, 64],     // #222940
-  [220, 227, 226],  // #DCE3E2
-  [78, 116, 166],   // #4E74A6
-  [132, 164, 191],  // #84A4BF
-  [21, 38, 18]      // #152612
+  [34, 41, 64], // #222940
+  [220, 227, 226], // #DCE3E2
+  [78, 116, 166], // #4E74A6
+  [132, 164, 191], // #84A4BF
+  [21, 38, 18] // #152612
 ];
 
 let fondo = paleta[0];
@@ -15,10 +14,15 @@ let texto = paleta[1];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  pixelDensity(displayDensity() > 1.5 ? 1 : displayDensity()); // evitar sobrecarga retina en móvil
+  cantidad = windowWidth < 768 ? 40 : 80; // Menos luces en móvil
+
   for (let i = 0; i < cantidad; i++) {
     luces.push(new Luz());
   }
-  textFont('Arial', 16);
+
+  textFont('Arial');
+  noStroke();
 }
 
 function windowResized() {
@@ -26,8 +30,7 @@ function windowResized() {
 }
 
 function draw() {
-  // Fondo sin transparencia para limpiar cada frame (sin rastro)
-  background(fondo[0], fondo[1], fondo[2]);
+  background(fondo[0], fondo[1], fondo[2], 50); // levemente translúcido para sensación suave
 
   for (let l of luces) {
     l.actualizar();
@@ -39,35 +42,41 @@ function draw() {
 
 class Luz {
   constructor() {
+    this.reset();
+  }
+
+  reset() {
     this.x = random(width);
     this.y = random(height);
-    this.dx = random(-0.5, 0.5);
-    this.dy = random(-0.5, 0.5);
+    this.dx = random(-0.4, 0.4);
+    this.dy = random(-0.4, 0.4);
     this.c = random(paleta);
   }
 
   actualizar() {
-    this.x += this.dx + random(-0.3, 0.3);
-    this.y += this.dy + random(-0.3, 0.3);
+    this.x += this.dx;
+    this.y += this.dy;
 
+    // Rebotan
     if (this.x < 0 || this.x > width) this.dx *= -1;
     if (this.y < 0 || this.y > height) this.dy *= -1;
   }
 
   mostrar() {
     let distancia = dist(this.x, this.y, mouseX, mouseY);
-    let tam = map(distancia, 0, 300, 12, 3);
-    let brillo = map(distancia, 0, 300, 255, 50);
+    let tam = map(distancia, 0, 300, 10, 2);
+    let brillo = map(distancia, 0, 300, 200, 50);
 
     fill(this.c[0], this.c[1], this.c[2], brillo);
-    noStroke();
     ellipse(this.x, this.y, tam, tam);
   }
 }
 
 function mostrarTextoPoetico() {
-  fill(texto[0], texto[1], texto[2], 180);
+  fill(texto[0], texto[1], texto[2], 160);
   textAlign(CENTER);
-  textSize(16);
+  textSize(width < 600 ? 12 : 16); // texto más pequeño en móvil
   text("A veces, la luz vaga sin rumbo, buscando un recuerdo", width / 2, height - 40);
 }
+
+
